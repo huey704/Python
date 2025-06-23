5d987eed9d3e62539110bde40628499c08eaf888
Arturo <arturotorresdamian1@gmail.com>
2025-06-23 16:14:27 -0700

    assetto_corsa_project_alpha
    Trying to add scroll bar but require more research. Got the GUI to work but still need more refinement to make it look better.

--- assetto_corsa_project.py
+++ assetto_corsa_project.py
@@ -1,4 +1,15 @@
 from configparser import ConfigParser
+from tkinter import *
+
+root = Tk()
+
+root.title("Assetto Corsa Project")
+root.geometry('900x850')
+
+scroll_bar = Scrollbar(root)
+scroll_bar.pack(side=RIGHT, fill=Y)
+mylist = Listbox(root, yscrollcommand=scroll_bar.set)
+
 
 # loads the ini file
 config = ConfigParser()
@@ -9,10 +20,20 @@ for section in config.sections():
     date = (config[section]['DATE'])
     time = (config[section]['TIME'])
 
-    # prints everything out
-    print(f'\n Car & Track: {section}')
-    print(f'\n Date and Time of Race: {date}')
-    print(f'\n Lap Time: {time}')
+# prints everything out
+    # print(f'\n Car & Track: {section}')
+    # print(f'\n Date and Time of Race: {date}')
+    # print(f'\n Lap Time: {time}')
+    lbl = Label(root, text=f'\n Car & Track: {section}')
+    lbl.pack()
+    lbl2 = Label(root, text=f'\n Date and Time of Race: {date}')
+    lbl2.pack()
+    lbl3 = Label(root, text=f'\n Lap Time: {time}')
+    lbl3.pack()
+
+mylist.pack(side=LEFT, fill=BOTH)
+scroll_bar.config(command=mylist.yview)
+root.mainloop()
 
 
 # print(config['KS_PORSCHE_911_GT1@KS_NORDSCHLEIFE-TOURISTENFAHRTEN'])

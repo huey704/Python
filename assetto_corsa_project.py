016d398b909d0cc714c739a8dbf9cd55b49765b1
Arturo <arturotorresdamian1@gmail.com>
2025-06-23 22:17:16 -0700

    V1_assetto_corsa_project
    Just moved some comments around, same code as previous v1.

--- assetto_corsa_project.py
+++ assetto_corsa_project.py
@@ -7,25 +7,20 @@ root = Tk()
 root.title("Assetto Corsa Project")
 root.geometry('900x850')
 
-# scroll_bar = Scrollbar(root)
-# scroll_bar.pack(side=RIGHT, fill=Y)
-# mylist = Listbox(root, yscrollcommand=scroll_bar.set)
-
 # loads the ini file
 config = ConfigParser()
 config.read(r'C:\Users\artur\OneDrive\Documents\Assetto Corsa\personalbest.ini')
 # breakpoint()
-# loop through a verison of the sorted ini file for car, track, date, time
+
 
 # converts the miliseconds to minutes and seconds, and miliseconds
-
-
 def convertMiliseconds(time_ms):
     total_seconds, miliseconds = divmod(time_ms, 1000)
     minutes, seconds = divmod(total_seconds, 60)
     return f"{minutes}:{seconds:02d}:{miliseconds:03d}"
 
 
+# loop through a verison of the sorted ini file for car, track, date, time
 for section in sorted(config.sections()):
     date_of_race = int(config[section]['DATE'])//1000
     raw_lap_time = int(config[section]['TIME'])
@@ -35,18 +30,16 @@ for section in sorted(config.sections()):
     date_of_race = datetime.datetime.fromtimestamp(date_of_race, datetime.UTC)
 
     # allows the text to be printed with editted size, font, and bold and location to be on the left side
-    car_track_label = Label(
-        root, text=f'\n Car & Track: {section}', anchor='w', justify=LEFT, font=('Arial', 10, 'bold'))
-    car_track_label.pack(anchor='w', padx=20, pady=2)
+    # car_track_label = Label(
+    #     root, text=f'\n Car & Track: {section}', anchor='w', justify=LEFT, font=('Arial', 10, 'bold'))
+    # car_track_label.pack(anchor='w', padx=20, pady=2)
 
-    date_time_label = Label(
-        root, text=f'\n Date and Time of Race: {date_of_race}', anchor='w', justify=LEFT, font=('Arial', 10, 'bold'))
-    date_time_label.pack(anchor='w', padx=20, pady=2)
+    # date_time_label = Label(
+    #     root, text=f'\n Date and Time of Race: {date_of_race}', anchor='w', justify=LEFT, font=('Arial', 10, 'bold'))
+    # date_time_label.pack(anchor='w', padx=20, pady=2)
 
-    lap_time_label = Label(
-        root, text=f'\n Lap Time: {formatted_lap_time}', anchor='w', justify=LEFT, font=('Arial', 10, 'bold'))
-    lap_time_label.pack(anchor='w', padx=20, pady=2)
+    # lap_time_label = Label(
+    #     root, text=f'\n Lap Time: {formatted_lap_time}', anchor='w', justify=LEFT, font=('Arial', 10, 'bold'))
+    # lap_time_label.pack(anchor='w', padx=20, pady=2)
 
-# mylist.pack(side=LEFT, fill=BOTH)
-# scroll_bar.config(command=mylist.yview)
 root.mainloop()
